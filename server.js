7f036e9f74d3f9e8bb848cdf42a84c620dbc59e1
makakoa <makakoa15@gmail.com>
2014-08-16 17:06:44 -0700

    ts the options req

--- server.js
+++ server.js
@@ -92,6 +92,7 @@ var deleteUser = function(req, res) {
 
 app.all('*', function(req, res, next) {
 	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
 	res.header("Access-Control-Allow-Headers", "X-Requested-With");
 	res.header("Content-Type", "application/json");
 	next();
